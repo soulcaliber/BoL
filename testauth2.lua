a942fa56e87997d5cafa52221053fa2a49f8dce5
Xgs <kyuriousol@gmail.com>
2014-09-26 11:00:17 -0700

    testauth2

---
+++ testauth2.lua
@@ -0,0 +1,11 @@
+function GetUTC()
+	return os.time()
+end
+
+--local clipText = GetClipboardText()
+local timestamp = GetUTC()
+local username = GetUser()
+
+--print("Text: " .. clipText)
+print("Timestamp: " .. timestamp)
+print("Username: " .. username)
